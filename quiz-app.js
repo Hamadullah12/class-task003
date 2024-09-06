49f5945fab912c3e065ea6951f98cd86ede1a799
Hamadullah12 <hamadfms05@gmail.com>
2024-09-06 10:45:53 +0500

    option added

--- quiz-app.js
+++ quiz-app.js
@@ -59,6 +59,8 @@ let option4 = document.getElementById("opt4");
 let answers = document.querySelectorAll(".options");
 
 let num = 0;
+let score = 0;
+let myScore = document.querySelector(".myScore");
 
 question.innerHTML = data[num].Q;
 option1.innerHTML = data[num].a;
@@ -66,23 +68,36 @@ option2.innerHTML = data[num].b;
 option3.innerHTML = data[num].c;
 option4.innerHTML = data[num].d;
 
-function checkAns(){
-let ans;
-answers.forEach((curElement)=>{
-if(curElement.checked){
-    ans = curElement.id;
-}
-})
+function checkAns() {
+    let ans;
+    answers.forEach((curElement) => {
+        if (curElement.checked) {
+            ans = curElement.id;
+            console.log(ans);
+        }
 
+    })
+    return ans;
 }
 
 function next() {
+
+    let checkedAns = checkAns()
+
+
+    if(checkedAns == data[num].ans){
+        score++;
+    }
     num++;
+
+    if(num < data.length){
     question.innerHTML = data[num].Q;
     option1.innerHTML = data[num].a;
     option2.innerHTML = data[num].b;
     option3.innerHTML = data[num].c;
     option4.innerHTML = data[num].d;
-
+} else{
+    myScore.innerHTML=`You Scored ${score}/${data.length} <br/> `
+}
 
 }
